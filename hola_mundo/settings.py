e6719a395f094da268688b674f9699079366a6b4
kennya2709 <118919711+kennya2709@users.noreply.github.com>
2023-11-10 16:39:45 -0600

    Update settings.py

--- hola_mundo/settings.py
+++ hola_mundo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5d*(dyv(mmcokpja%&=!#9g+of(e&gngynlt@49mybwagc(q&#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.142.50.180']
 
 
 # Application definition
